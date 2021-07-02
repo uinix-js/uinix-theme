2613609cbe366773e1e2fd0a46a015a2fa6b6258
chrisrzhou <chrisrzhou@pm.me>
2021-07-01 23:48:48 -0700

    refactor: cleanup types and export types to index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+/**
+ * @typedef {import('./lib/types.js').Theme} Theme
+ * @typedef {import('./lib/types.js').ThemeMapping} ThemeMapping
+ */
+
 export {createTheme} from './lib/create-theme.js';
 export {themeMapping} from './lib/theme-mapping.js';
