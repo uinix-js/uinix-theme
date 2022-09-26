90a5e07e6403fcf33ddfc446739ea14ab43b95fa
chrisrzhou <chrisrzhou@pm.me>
2022-09-26 00:12:33 -0700

    refactor: codestyle (move type imports to top of file)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export {combineStyles} from './lib/combine-styles.js';
 export {createCssVariables} from './lib/create-css-variables.js';
 export {createThemeRenderer} from './lib/create-theme-renderer.js';
+
 export * from './lib/types.js';
